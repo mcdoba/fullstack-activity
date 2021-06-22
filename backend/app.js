67bd359fb8db78febb724fd27daa5b4d1297e9ba
mcdoba <quentin.guillouet@gmail.com>
2021-06-22 13:48:13 +0200

    router

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const Product = require('./product');
+const Product = require('./models/product');
+const stuffRoutes = require('./routes/stuff');
 
 
 const mongoose = require('mongoose');
@@ -20,41 +21,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(bodyParser.json());
-
-app.post('/api/products', (req, res, next) => {
-    delete req.body._id;
-    const product = new Product({
-      ...req.body
-    });
-    product.save()
-      .then(() => res.status(201).json({product}))
-      .catch(error => res.status(400).json({ error }));
-  });
   
-  app.get('/api/products/:id', (req, res, next) => {
-    Product.findOne({ _id: req.params.id })
-      .then(product => res.status(200).json({product}))
-      .catch(error => res.status(404).json({ error }));
-  });
-  
-  app.put('/api/products/:id', (req, res, next) => {
-    Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Modified!'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-  
-  app.delete('/api/products/:id', (req, res, next) => {
-    Product.deleteOne({ _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Deleted!'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-  
-  app.use('/api/products', (req, res, next) => {
-    Product.find()
-      .then(products => res.status(200).json({products}))
-      .catch(error => res.status(400).json({ error }));
-  });
-  
- 
+app.use('/api/stuff', stuffRoutes);
   
   module.exports = app;
